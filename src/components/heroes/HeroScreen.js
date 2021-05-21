dec556351f49790073bbe627ba95a30f4c6167a8
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2021-05-21 16:59:13 -0500

    Leer argumentos por URL

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,27 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router'
+import { getHeroesById as getHeroById } from '../../selectors/getHeroById';
 
 const HeroScreen = () => {
+
+    const { heroId } = useParams();
+
+    const hero = getHeroById( heroId );
+
+    if ( !hero ) {
+        return <Redirect to="/" />;
+    }
+
+    const {
+        id,
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters
+    } = hero;
+    
+
     return (
         <div>
             <h1>HeroScreen</h1>
