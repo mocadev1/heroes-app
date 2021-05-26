7958f83e24d484f4811ac524f49a86ee1d360d2c
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2021-05-26 18:35:02 -0500

    Context y Reducer de la aplicación, Login, Logout

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 const LoginScreen = ({ history }) => {
 
+    const { dispatch } = useContext(AuthContext)
 
     const handleLogin = () => {
+
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Omar'
+            }
+        });
+
         history.replace('/');
     };
     
