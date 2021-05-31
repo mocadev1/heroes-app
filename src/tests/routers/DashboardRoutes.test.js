4af2d2ecc24752106b8b36fddf1fa2f426b8fe13
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2021-05-31 17:25:00 -0500

    Pruebas en el DashboardRoutes

---
+++ src/tests/routers/DashboardRoutes.test.js
@@ -0,0 +1,29 @@
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import { AuthContext } from '../../auth/AuthContext';
+import DashboardRoutes from '../../routers/DashboardRoutes';
+
+describe('<DashboardRoutes /> tests', () => {
+    test('should render properly', () => {
+        
+        const contextValue = {
+            dispatch: jest.fn(),
+            user: {
+                logged: true,
+                name: 'Omar'
+            }
+        }
+        
+        const wrapper = mount(
+            <AuthContext.Provider value={ contextValue }>
+                <MemoryRouter>
+                    <DashboardRoutes />
+                </MemoryRouter>
+            </AuthContext.Provider>
+            );
+        
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('.text-info').text().trim() ).toBe('Omar');
+    });
+    
+});
